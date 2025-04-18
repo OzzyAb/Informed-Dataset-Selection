0210b1e60f810e449d1fc01d2187d9c14a000b53
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-04-18 18:09:10 +0200

    add cors

--- datasets.recommender-systems.com/index.php
+++ datasets.recommender-systems.com/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
 include_once('apis/db-conn.php');
 include_once('apis/algorithm.php');
 include_once('apis/dataset.php');
